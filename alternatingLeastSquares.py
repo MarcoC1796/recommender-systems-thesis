4ad8e0e9ed51f82fabfce2bfeeb3cf0a2aa2bab6
Marco Chacon <marco.chacon1796@gmail.com>
2023-04-18 02:29:38 -0600

    adding biases to model

--- alternatingLeastSquares.py
+++ alternatingLeastSquares.py
@@ -1,16 +1,27 @@
 import numpy as np
 from gradientDescent import gradientDescent
-from cost_functions import get_Ju_and_DJu, get_Ja_and_DJa
+from cost_functions import get_Ju_and_DJu, get_Ja_and_DJa, get_alpha, get_beta
 from aux_functions import initializeQ, initializeP
 from accuracyMetrics import RMSE
 
 
-def getRMSEs_Pfixed(newQs, P, R):
+def getRMSEs_Pfixed(
+    newQs, P, R, muestral_bias=False, parameter_bias=False, alpha=None, beta=None
+):
+    if muestral_bias and parameter_bias:
+        raise Exception("muestral_bias and parameter_bias can not be both true")
     RMSEs = []
-    for Q_k in newQs:
+    for i, Q_k in enumerate(newQs):
         Q_k = Q_k.reshape(R.shape[0], -1)
-        errors = (Q_k @ P.T - R).flatten()
-
+        if not (muestral_bias or parameter_bias):
+            errors = (Q_k @ P.T - R).flatten()
+        elif muestral_bias:
+            eta = alpha[:, np.newaxis] + beta
+            errors = (eta + Q_k @ P.T - R).flatten()
+        else:
+            curr_alpha = alpha[i]
+            eta = curr_alpha[:, np.newaxis] + beta
+            errors = (eta + Q_k @ P.T - R).flatten()
         # Check for NaN values using np.isnan()
         is_nan = np.isnan(errors)
 
@@ -21,12 +32,23 @@ def getRMSEs_Pfixed(newQs, P, R):
     return RMSEs
 
 
-def getRMSEs_Qfixed(newPs, Q, R):
+def getRMSEs_Qfixed(
+    newPs, Q, R, muestral_bias=False, parameter_bias=False, alpha=None, beta=None
+):
+    if muestral_bias and parameter_bias:
+        raise Exception("muestral_bias and parameter_bias can not be both true")
     RMSEs = []
-    for P_k in newPs:
+    for i, P_k in enumerate(newPs):
         P_k = P_k.reshape(R.shape[1], -1)
-        errors = (Q @ P_k.T - R).flatten()
-
+        if not (muestral_bias or parameter_bias):
+            errors = (Q @ P_k.T - R).flatten()
+        elif muestral_bias:
+            eta = alpha[:, np.newaxis] + beta
+            errors = (eta + Q @ P_k.T - R).flatten()
+        else:
+            curr_beta = beta[i]
+            eta = alpha[:, np.newaxis] + curr_beta
+            errors = (eta + Q @ P_k.T - R).flatten()
         # Check for NaN values using np.isnan()
         is_nan = np.isnan(errors)
 
@@ -37,7 +59,23 @@ def getRMSEs_Qfixed(newPs, Q, R):
     return RMSEs
 
 
-def ALS(R, f, lambQ=0, lambP=0, alternations=1, tol=1e-3, max_iter=1e3):
+def ALS(
+    R,
+    f,
+    lambQ=0,
+    lambP=0,
+    muestral_bias=False,
+    parameter_bias=False,
+    alternations=1,
+    tol=1e-3,
+    max_iter=1e3,
+):
+    if muestral_bias and parameter_bias:
+        raise Exception("muestral_bias and parameter_bias can not be both true")
+    elif muestral_bias or parameter_bias:
+        alpha = get_alpha(R)
+        beta = get_beta(R)
+
     Q = initializeQ(R.shape[0], f)
     P = initializeP(R.shape[1], f)
 
@@ -53,29 +91,97 @@ def ALS(R, f, lambQ=0, lambP=0, alternations=1, tol=1e-3, max_iter=1e3):
         / np.absolute(Ja_alt_values[-2][-1])
         > tol
     ):
-        # Optimizing with respect to Q
-        Ju, DJu = get_Ju_and_DJu(R, P, f, lambQ, lambP)
-        Q = Q.flatten()
-        gradientDescentResults = gradientDescent(Ju, Q, DJu, tol, max_iter)
-        Q = gradientDescentResults["x_values"][-1].reshape(R.shape[0], -1)
-        Ju_values = gradientDescentResults["func_values"]
-        newQs = gradientDescentResults["x_values"]
-        Ju_alt_values.append(Ju_values)
-        RMSEu_values.append(getRMSEs_Pfixed(newQs, P, R))
+        if muestral_bias:
+            # Optimizing with respect to Q
+            Ju, DJu = get_Ju_and_DJu(R, P, f, lambQ, lambP, muestral_bias=muestral_bias)
+            Q = Q.flatten()
+            gradientDescentResults = gradientDescent(Ju, Q, DJu, tol, max_iter)
+            Q = gradientDescentResults["x_values"][-1].reshape(R.shape[0], -1)
+            Ju_values = gradientDescentResults["func_values"]
+            newQs = gradientDescentResults["x_values"]
+            Ju_alt_values.append(Ju_values)
+            RMSE_values_muestral_biased = getRMSEs_Pfixed(
+                newQs, P, R, muestral_bias=True, alpha=alpha, beta=beta
+            )
+            RMSEu_values.append(RMSE_values_muestral_biased)
 
-        # Optimizing with respect to P
-        Ja, DJa = get_Ja_and_DJa(R, Q, f, lambQ, lambP)
-        P = P.flatten()
-        gradientDescentResults = gradientDescent(Ja, P, DJa, tol, max_iter)
-        P = gradientDescentResults["x_values"][-1].reshape(R.shape[1], -1)
-        Ja_values = gradientDescentResults["func_values"]
-        newPs = gradientDescentResults["x_values"]
-        Ja_alt_values.append(Ja_values)
-        RMSEa_values.append(getRMSEs_Qfixed(newPs, Q, R))
+            # Optimizing with respect to P
+            Ja, DJa = get_Ja_and_DJa(R, Q, f, lambQ, lambP, muestral_bias=muestral_bias)
+            P = P.flatten()
+            gradientDescentResults = gradientDescent(Ja, P, DJa, tol, max_iter)
+            P = gradientDescentResults["x_values"][-1].reshape(R.shape[1], -1)
+            Ja_values = gradientDescentResults["func_values"]
+            newPs = gradientDescentResults["x_values"]
+            Ja_alt_values.append(Ja_values)
+            RMSE_values_muestral_biased = getRMSEs_Qfixed(
+                newPs, Q, R, muestral_bias=True, alpha=alpha, beta=beta
+            )
+            RMSEa_values.append(RMSE_values_muestral_biased)
+
+        elif parameter_bias:
+            # Optimizing with respect to Q
+            Ju, DJu = get_Ju_and_DJu(
+                R, P, f, lambQ, lambP, parameter_bias=parameter_bias, beta=beta
+            )
+            Q = Q.flatten()
+            Theta = np.append(alpha, Q)
+            gradientDescentResults = gradientDescent(Ju, Theta, DJu, tol, max_iter)
+            newThetas = gradientDescentResults["x_values"]
+            Q = newThetas[-1][R.shape[0] :].reshape(R.shape[0], -1)
+            alpha = newThetas[-1][: R.shape[0]]
+            Ju_values = gradientDescentResults["func_values"]
+            newQs = np.array(newThetas)[:, R.shape[0] :]
+            alphas = np.array(newThetas)[:, : R.shape[0]]
+            Ju_alt_values.append(Ju_values)
+            RMSE_values_parameter_biased = getRMSEs_Pfixed(
+                newQs, P, R, parameter_bias=True, alpha=alphas, beta=beta
+            )
+            RMSEu_values.append(RMSE_values_parameter_biased)
+
+            # Optimizing with respect to P
+            Ja, DJa = get_Ja_and_DJa(
+                R, Q, f, lambQ, lambP, parameter_bias=parameter_bias, alpha=alpha
+            )
+            P = P.flatten()
+            Theta = np.append(beta, P)
+            gradientDescentResults = gradientDescent(Ja, Theta, DJa, tol, max_iter)
+            newThetas = gradientDescentResults["x_values"]
+            P = newThetas[-1][R.shape[1] :].reshape(R.shape[1], -1)
+            beta = newThetas[-1][: R.shape[1]]
+            Ja_values = gradientDescentResults["func_values"]
+            newPs = np.array(newThetas)[:, R.shape[1] :]
+            betas = np.array(newThetas)[:, : R.shape[1]]
+            Ja_alt_values.append(Ja_values)
+            RMSE_values_parameter_biased = getRMSEs_Qfixed(
+                newPs, Q, R, parameter_bias=True, alpha=alpha, beta=betas
+            )
+            RMSEa_values.append(RMSE_values_parameter_biased)
+        else:
+            # Optimizing with respect to Q
+            Ju, DJu = get_Ju_and_DJu(R, P, f, lambQ, lambP)
+            Q = Q.flatten()
+            gradientDescentResults = gradientDescent(Ju, Q, DJu, tol, max_iter)
+            Q = gradientDescentResults["x_values"][-1].reshape(R.shape[0], -1)
+            Ju_values = gradientDescentResults["func_values"]
+            newQs = gradientDescentResults["x_values"]
+            Ju_alt_values.append(Ju_values)
+            RMSEu_values.append(getRMSEs_Pfixed(newQs, P, R))
+
+            # Optimizing with respect to P
+            Ja, DJa = get_Ja_and_DJa(R, Q, f, lambQ, lambP)
+            P = P.flatten()
+            gradientDescentResults = gradientDescent(Ja, P, DJa, tol, max_iter)
+            P = gradientDescentResults["x_values"][-1].reshape(R.shape[1], -1)
+            Ja_values = gradientDescentResults["func_values"]
+            newPs = gradientDescentResults["x_values"]
+            Ja_alt_values.append(Ja_values)
+            RMSEa_values.append(getRMSEs_Qfixed(newPs, Q, R))
 
     return {
         "Q": Q,
         "P": P,
+        "alpha": alpha if muestral_bias or parameter_bias else None,
+        "beta": beta if muestral_bias or parameter_bias else None,
         "Ju_values": Ju_alt_values,
         "Ja_values": Ja_alt_values,
         "RMSEu_values": RMSEu_values,
--- cost_functions.py
+++ cost_functions.py
@@ -1,55 +1,193 @@
 import numpy as np
 
 
-def get_Ju_and_DJu(R, P, f, lambQ=0, lambP=0):
+def get_alpha(R):
+    return np.nansum(R, 1) / np.count_nonzero(~np.isnan(R), 1)
+
+
+def get_beta(R):
+    return np.nansum(R, 0) / np.count_nonzero(~np.isnan(R), 0)
+
+
+def get_muestral_biases(R):
+    alpha = get_alpha(R)
+    beta = get_beta(R)
+    eta = alpha[:, np.newaxis] + beta
+    return eta
+
+
+def get_Ju_and_DJu(
+    R, P, f, lambQ=0, lambP=0, muestral_bias=False, parameter_bias=False, beta=None
+):
     """
     Arguments:
         R: m P n matriP.
         P: n P p matriP
     """
+    if muestral_bias and parameter_bias:
+        raise Exception("muestral_bias and parameter_bias can not be both true")
+    elif parameter_bias and beta is None:
+        raise Exception("if parameter_bias is true, beta must be specified")
 
     m, _ = R.shape
 
-    def Ju(Q):
-        """
-        Arguments:
-            Q: a flattened array representing a m x p matrix
-        """
-        Q = Q.reshape((m, f))
-        return (
-            np.nansum(np.square(Q @ P.T - R)) / 2
-            + (lambQ / 2) * np.nansum(np.square(Q))
-            + (lambP / 2) * np.nansum(np.square(P))
-        )
+    if muestral_bias:
+        eta = get_muestral_biases(R)
 
-    def DJu(Q):
-        """
-        Arguments:
-            Q: a flattened array representing a m x p matrix
-        """
-        Q = Q.reshape((m, f))
-        E = np.nan_to_num(Q @ P.T - R)
-        DJu_values = E @ P + lambQ * Q
-        return DJu_values.flatten()
+        def Ju(Q):
+            """
+            Arguments:
+                Q: a flattened array representing a m x p matrix
+            """
+            Q = Q.reshape((m, f))
+            return (
+                np.nansum(np.square(eta + Q @ P.T - R)) / 2
+                + (lambQ / 2) * np.nansum(np.square(Q))
+                + (lambP / 2) * np.nansum(np.square(P))
+            )
+
+        def DJu(Q):
+            """
+            Arguments:
+                Q: a flattened array representing a m x p matrix
+            """
+            Q = Q.reshape((m, f))
+            E = np.nan_to_num(eta + Q @ P.T - R)
+            DJu_values = E @ P + lambQ * Q
+            return DJu_values.flatten()
+
+    elif parameter_bias:
+
+        def Ju(Theta):
+            """
+            Arguments:
+                Q: a flattened array representing a m x p matrix
+            """
+            alpha = Theta[:m]
+            Q = Theta[m:]
+            Q = Q.reshape((m, f))
+            eta = alpha[:, np.newaxis] + beta
+
+            return (
+                np.nansum(np.square(eta + Q @ P.T - R)) / 2
+                + (lambQ / 2) * np.nansum(np.square(Q))
+                + (lambP / 2) * np.nansum(np.square(P))
+            )
+
+        def DJu(Theta):
+            """
+            Arguments:
+                Q: a flattened array representing a m x p matrix
+            """
+            alpha = Theta[:m]
+            Q = Theta[m:]
+            Q = Q.reshape((m, f))
+
+            eta = alpha[:, np.newaxis] + beta
+            E = np.nan_to_num(eta + Q @ P.T - R)
+            DJu_DQ = E @ P + lambQ * Q
+            DJu_Dalpha = np.nansum(E, 1)
+
+            DJu_values = np.append(DJu_Dalpha.flatten(), DJu_DQ)
+
+            return DJu_values
+
+    else:
+
+        def Ju(Q):
+            """
+            Arguments:
+                Q: a flattened array representing a m x p matrix
+            """
+            Q = Q.reshape((m, f))
+
+            return (
+                np.nansum(np.square(Q @ P.T - R)) / 2
+                + (lambQ / 2) * np.nansum(np.square(Q))
+                + (lambP / 2) * np.nansum(np.square(P))
+            )
+
+        def DJu(Q):
+            """
+            Arguments:
+                Q: a flattened array representing a m x p matrix
+            """
+            Q = Q.reshape((m, f))
+            E = np.nan_to_num(Q @ P.T - R)
+            DJu_values = E @ P + lambQ * Q
+            return DJu_values.flatten()
 
     return Ju, DJu
 
 
-def get_Ja_and_DJa(R, Q, f, lambQ=0, lambP=0):
+def get_Ja_and_DJa(
+    R, Q, f, lambQ=0, lambP=0, muestral_bias=False, parameter_bias=False, alpha=None
+):
+    if muestral_bias and parameter_bias:
+        raise Exception("muestral_bias and parameter_bias can not be both true")
+    elif parameter_bias and alpha is None:
+        raise Exception("if parameter_bias is true, beta must be specified")
     _, n = R.shape
 
-    def Ja(P):
-        P = P.reshape((n, f))
-        return (
-            np.nansum(np.square(Q @ P.T - R)) / 2
-            + (lambQ / 2) * np.nansum(np.square(Q))
-            + (lambP / 2) * np.nansum(np.square(P))
-        )
+    if muestral_bias:
+        eta = get_muestral_biases(R)
 
-    def DJa(P):
-        P = P.reshape((n, f))
-        E = np.nan_to_num(Q @ P.T - R)
-        DJa_values = E.T @ Q + lambP * P
-        return DJa_values.flatten()
+        def Ja(P):
+            P = P.reshape((n, f))
+            return (
+                np.nansum(np.square(eta + Q @ P.T - R)) / 2
+                + (lambQ / 2) * np.nansum(np.square(Q))
+                + (lambP / 2) * np.nansum(np.square(P))
+            )
+
+        def DJa(P):
+            P = P.reshape((n, f))
+            E = np.nan_to_num(eta + Q @ P.T - R)
+            DJa_values = E.T @ Q + lambP * P
+            return DJa_values.flatten()
+
+    elif parameter_bias:
+
+        def Ja(Theta):
+            beta = Theta[:n]
+            P = Theta[n:]
+            P = P.reshape((n, f))
+            eta = alpha[:, np.newaxis] + beta
+
+            return (
+                np.nansum(np.square(eta + Q @ P.T - R)) / 2
+                + (lambQ / 2) * np.nansum(np.square(Q))
+                + (lambP / 2) * np.nansum(np.square(P))
+            )
+
+        def DJa(Theta):
+            beta = Theta[:n]
+            P = Theta[n:]
+            P = P.reshape((n, f))
+
+            eta = alpha[:, np.newaxis] + beta
+            E = np.nan_to_num(eta + Q @ P.T - R)
+            DJa_DQ = E.T @ Q + lambP * P
+            DJa_Dalpha = np.nansum(E, 0)
+
+            DJa_values = np.append(DJa_Dalpha.flatten(), DJa_DQ)
+
+            return DJa_values.flatten()
+
+    else:
+
+        def Ja(P):
+            P = P.reshape((n, f))
+            return (
+                np.nansum(np.square(Q @ P.T - R)) / 2
+                + (lambQ / 2) * np.nansum(np.square(Q))
+                + (lambP / 2) * np.nansum(np.square(P))
+            )
+
+        def DJa(P):
+            P = P.reshape((n, f))
+            E = np.nan_to_num(Q @ P.T - R)
+            DJa_values = E.T @ Q + lambP * P
+            return DJa_values.flatten()
 
     return Ja, DJa
